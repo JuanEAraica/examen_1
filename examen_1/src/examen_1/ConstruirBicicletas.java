8083a63b7db7402f8a06b89f5a3c13fd377ea353
Aray <Aray@DESKTOP-RCUIACQ>
2019-04-05 18:17:26 -0600

    uno mas

---
+++ examen_1/src/examen_1/ConstruirBicicletas.java
@@ -0,0 +1,11 @@
+package examen_1;
+
+public class ConstruirBicicletas {
+
+	public static void main(String[] args) {// se convierte en clase main
+		
+
+	}
+
+}
+
--- examen_1/src/examen_1/classMarco.java
+++
@@ -1,5 +0,0 @@
-package examen_1;
-
-public class classMarco {
-
-}
